995b8c14b01eea1abf00052d2b3f8543307768ef
Carlos Eduardo de Paula Stachissini <92668640+Stachiss@users.noreply.github.com>
2024-09-30 20:17:56 +0000

    commit

--- app.py
+++ app.py
@@ -39,11 +39,11 @@ def previsao(interpreter,image):
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
 
-    interpreter.set_tesnor(input_details[0]['index'],image)
+    interpreter.set_tensor(input_details[0]['index'],image)
 
     interpreter.invoke()
 
-    output_data = interpreter.get_tesnor(output_details[0]['index'])
+    output_data = interpreter.get_tensor(output_details[0]['index'])
     classes = ['BlackMeasles', 'BlackRot', 'HealthyGrapes', 'LeafBlight']
 
     df = pd.DataFrame()
